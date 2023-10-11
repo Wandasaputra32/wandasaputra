16733c72f6507cab604cd662e5d3424ff020c671
riza20688 <riza.20688@gmail.com>
2023-10-11 23:22:16 +0700

    Tugas Pertemuan 2

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -6,13 +6,13 @@ class Latihan1 extends CI_Controller
         echo "Selamat Datang.. selamat belajar Web Programming";
         //$this->load->view('view-latihan');
     }
-    public function penjumlahan($n1, $n2)
+    public function penjumlahan($nil1, $nil2)
     {
         $this->load->model('Model_latihan1');
 
-        $data['nilai1'] = $n1;
-        $data['nilai2'] = $n2;
-        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+        $data['nilai1'] = $nil1;
+        $data['nilai2'] = $nil2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($nil1, $nil2);
 
         $this->load->view('view-latihan', $data);
     }
