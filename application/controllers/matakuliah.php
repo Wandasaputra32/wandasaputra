be0cb96bdb34d7eba883d3d7ba5a57baa383967a
riza20688 <riza.20688@gmail.com>
2023-10-16 23:57:59 +0700

    tugas Pertemuan 2 Contoh 5

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -9,12 +9,12 @@ class matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data1 = [
+        $data = [
             'kode' => $this->input->post('kode'),
             'nama' => $this->input->post('nama'),
             'sks' => $this->input->post('sks')
         ];
 
-        $this->load->view('view-data-matakuliah', $data1);
+        $this->load->view('view-data-matakuliah', $data);
     }
 }
