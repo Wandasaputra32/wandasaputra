3062d89678e623a7db4ed6890438c4eb56ed80bf
riza20688 <riza.20688@gmail.com>
2023-11-21 19:45:04 +0700

    Sampai Pertemuan 12

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class ModelBuku extends CI_Model
 {
     //manajemen buku
-    public function getBuku()
+    public function tampil()
     {
         return $this->db->get('buku');
     }
@@ -11,28 +11,28 @@ class ModelBuku extends CI_Model
     {
         return $this->db->get_where('buku', $where);
     }
-    public function simpanBuku($data = null)
+    public function simpan($data = null)
     {
-        $this->db->insert('buku',$data);
+        $this->db->insert('buku', $data);
     }
-    public function updateBuku($data = null, $where = null)
+    public function update($data = null, $where = null)
     {
         $this->db->update('buku', $data, $where);
     }
-    public function hapusBuku($where = null)
+    public function hapus($where = null)
     {
         $this->db->delete('buku', $where);
     }
     public function total($field, $where)
     {
         $this->db->select_sum($field);
-    if(!empty($where) && count($where) > 0){
-        $this->db->where($where);
-    }
+        if (!empty($where) && count($where) > 0) {
+            $this->db->where($where);
+        }
         $this->db->from('buku');
         return $this->db->get()->row($field);
     }
-    
+
     //manajemen kategori
     public function getKategori()
     {
@@ -59,7 +59,7 @@ class ModelBuku extends CI_Model
     {
         $this->db->select('buku.id_kategori,kategori.kategori');
         $this->db->from('buku');
-        $this->db->join('kategori','kategori.id = 
+        $this->db->join('kategori', 'kategori.id = 
         buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
